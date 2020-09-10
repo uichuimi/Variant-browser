83f430166a4177cefd41b3ee3cc923e99325f810
paulajt <paulajimte1997@gmail.com>
2020-09-10 11:18:11 +0100

    Creando el proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FiltersComponent } from './filters/filters.component';
+import { DetailsComponent } from './details/details.component';
+import { TableComponent } from './table/table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FiltersComponent,
+    DetailsComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
