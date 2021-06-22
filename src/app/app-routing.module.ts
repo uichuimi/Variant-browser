48db32ead800a7519505f4f0ec8559d0a041dbf0
Jesus Alberto A. Larez Romero <55491787+jesuslarez@users.noreply.github.com>
2021-06-22 18:58:14 +0100

    Creating the routing system

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { AppComponent } from './app.component';
+import { FrameComponent } from './components/frame/frame.component';
 
 const routes: Routes = [
+	{
+		path: 'home',
+		component: FrameComponent
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+	{
+		path: '**',
+		redirectTo: '/home'
+	}
 ];
 
 @NgModule({
