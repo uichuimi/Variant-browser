5394f28b722da18d094603decef558ef718e920b
Jaime <jaime99dr@gmail.com>
2022-01-24 11:33:30 +0000

    VB-7 documentacion token-storage.service.ts

--- src/app/services/token-storage.service.ts
+++ src/app/services/token-storage.service.ts
@@ -15,24 +15,44 @@ export class TokenStorageService {
 
   constructor() { }
 
+  /**
+   * Limpia todos los datos guardados en sessionStorage, 
+   * incluyendo el token y su valor, por lo que se cierra sesión
+   */
   signout() {
     window.sessionStorage.clear();
   }
 
+  /**
+   * Borra el token anterior y guarda el nuevo pasado por parámetro
+   * @param token token que se consigue al hacer login
+   */
   public saveToken(token: string) {
     window.sessionStorage.removeItem(TOKEN_KEY);
     window.sessionStorage.setItem(TOKEN_KEY, token);
   }
 
+  /**
+   * Devuelve el token que se está usando
+   * @returns token usado actualmente
+   */
   public getToken(): string {
     return sessionStorage.getItem(TOKEN_KEY);
   }
 
+  /**
+   * Borra el usuario anterior y guarda el actual
+   * @param user objeto con las propiedades username y password
+   */
   public saveUser(user) {
     window.sessionStorage.removeItem(USER_KEY);
     window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
   }
 
+  /**
+   * Devuelve el usuario actual parseando el objeto
+   * @returns el usuario actual
+   */
   public getUser() {
     return JSON.parse(sessionStorage.getItem(USER_KEY));
   }
