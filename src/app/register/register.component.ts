24ee59e2f438d60b5ff89d93e7f064a0c87cba39
Jaime <jaime99dr@gmail.com>
2022-01-26 19:03:45 +0000

    VB-23 documentar register.component.t

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,22 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from '../services/auth.service';
 
+/**
+ * Hace que la clase se pueda usar como componente
+ */
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
+/**
+ * Esta clase se encarga de registrar a un usuario
+ * implementa el lifecycle hook OnInit
+ */
 export class RegisterComponent implements OnInit {
   form: any = {};
   isSuccessful = false;
   isSignUpFailed = false;
   errorMessage = '';
 
+  /**
+   * Crea un objeto tipo AuthService
+   * @param authService clase que gestiona el login y el registro de usuarios
+   */
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  /**
+   * Método que hace la llamada al endpoint /signup
+   * Se ejecuta al pulsar el botón del formulario
+   * de register.component.html
+   */
   onSubmit() {
     this.authService.register(this.form).subscribe(
       data => {
