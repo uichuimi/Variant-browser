624198dd97e6f49677547ae0d956ec279063e55d
Jaime <jaime99dr@gmail.com>
2022-03-09 19:51:00 +0000

    VarcanService test

--- src/app/var-can.service.spec.ts
+++ src/app/var-can.service.spec.ts
@@ -1,56 +1,185 @@
-import { TestBed } from '@angular/core/testing';
-import { LoginService } from './services/login.service';
-
 import { VarCanService } from './var-can.service';
+
+// Inputs and outputs mockups
+// LOGIN
+import * as loginCorrectMockup from '../fixtures/varcanService/login/input/loginCorrectMockup.json';
+import * as loginIncorrectMockup from '../fixtures/varcanService/login/input/loginIncorrectMockup.json';
+import * as loginIncorrectOutputMockup from '../fixtures/varcanService/login/output/loginIncorrectOutputMockup.json';
+
+// BIOTYPES
+import * as biotypeOutputMockup from '../fixtures/varcanService/biotype/output/biotypeOutputMockup.json';
+
+// CHROMOSOMES
+import * as chromosomeOutputMockup from '../fixtures/varcanService/chromosome/output/chromosomeOutputMockup.json';
+
+// IMPACTS
+import * as impactOutputMockup from '../fixtures/varcanService/impact/output/impactOutputMockup.json';
+
+// GENOTYPE_TYPE
+import * as genotypeTypeOutputMockup from '../fixtures/varcanService/genotypeType/output/genotypeTypeOutputMockup.json';
+
+// INDIVIDUALS
+import * as individualOutputMockup from '../fixtures/varcanService/individual/output/individualOutputMockup.json';
+
+// EFFECTS
+import * as effectOutputMockup from '../fixtures/varcanService/effect/output/effectOutputMockup.json';
+
+// POPULATIONS
+import * as populationOutputMockup from '../fixtures/varcanService/population/output/populationOutputMockup.json';
+
+// GENES
+import * as geneSingleParameterInputMockup from '../fixtures/varcanService/gene/input/geneSingleParameterInputMockup.json';
+import * as geneMultipleParametersInputMockup from '../fixtures/varcanService/gene/input/geneMutipleParametersInputMockup.json';
+import * as geneOutputMockup from '../fixtures/varcanService/gene/output/geneOutputMockup.json';
+import * as geneFilteredOutputMockup from '../fixtures/varcanService/gene/output/geneFilteredOutputMockup.json';
 
 describe('VarCanService', () => {
   let service: VarCanService;
-  let loginServiceSpy: jasmine.SpyObj<LoginService>;
 
   beforeEach(() => {
-    const spy = jasmine.createSpyObj('LoginService', ['login']);
-
-    TestBed.configureTestingModule({
-      providers: [
-        service,
-        { provide: LoginService, useValue: spy }
-      ]
-    });
-    service = TestBed.inject(VarCanService);
-    loginServiceSpy = TestBed.inject(LoginService) as jasmine.SpyObj<LoginService>;
+    service = new VarCanService();  // falta pasarle URL o AxiosInstance
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  /*it("#login should return token: <Token>", () => {
-    const loginModel: Login = { username: "uichuimi", password: "uichuimi01" };
-    const token: Token = { access_token: "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1aWNodWltaSIsImV4cCI6MTY0NjUxNDMyMCwiaWF0IjoxNjQ2NDc4MzIwfQ.    gZY1DohG7NfrXCwrngN6EJtD9KaMzEGG7Ef8yh3Gv6mBsbkVy4hay6hFIdERdx1dz0LX0XODto7aFaPUo93_lA" };
-    loginServiceSpy.login.and.returnValue(token);
-
-    expect(service.login(loginModel))
-      .withContext("service returned token")
-      .toBe(token);
-    expect(loginServiceSpy.login.calls.count())
-      .withContext('spy method was called once')
-      .toBe(1);
-    expect(loginServiceSpy.login.calls.mostRecent().returnValue)
-      .toBe(token);
+  it("should return token: <Token> (/login)", () => {
+    const loginModel: Login = loginCorrectMockup;
+    const result: Token = service.login(loginModel);
+    
+    expect(result['access_token'])
+      .withContext("service returned access_token")
+      .toBeTruthy()
+    expect(result['refresh_token'])
+      .withContext("service returned refresh_token")
+      .toBeTruthy();
+    expect(result['expires_in'])
+      .withContext("service returned expires_in")
+      .toBeTruthy();
+    expect(result['token_type'])
+      .withContext("service returned token_type")
+      .toBe('bearer');
   });
 
-  it("#login should return status: failed", () => {
-    const loginModel: Login = { username: "noExisto", password: "uichuimi" };
-    const status: ResponseStatus = { status: "failed" };
-    loginServiceSpy.login.and.returnValue(status);
+  it("should return status: failed (/login)", () => {
+    const loginModel: Login = loginIncorrectMockup;
+    const status: ResponseStatus = loginIncorrectOutputMockup;
 
     expect(service.login(loginModel))
       .withContext("service return status failed")
       .toBe(status);
-    expect(loginServiceSpy.login.calls.count())
-      .withContext('spy method was called once')
+  });
+
+  it("should return Array<Biotype> (/biotypes)", () => {
+    const result: Array<Biotype> = biotypeOutputMockup.biotypes;
+
+    expect(service.getBiotypes())
+      .withContext("service return Array<Biotype>")
+      .toBe(result);
+  });
+
+  it("should return Array<Chromosome> (/chromosomes)", () => {
+    const result: Array<Chromosome> = chromosomeOutputMockup.chromosomes;
+
+    expect(service.getChromosomes())
+      .withContext("service return Array<Chromosome>")
+      .toBe(result);
+  });
+
+  it("should return Array<Impact> (/impacts)", () => {
+    const result: Array<Impact> = impactOutputMockup.impacts;
+
+    expect(service.getImpacts())
+      .withContext("service return Array<Impact>")
+      .toBe(result);
+  }); 
+  
+  it("should return Array<GenotypeType> (/genotype_type)", () => {
+    const result: Array<GenotypeType> = genotypeTypeOutputMockup.genotypeType;
+
+    expect(service.getGenotypeType())
+      .withContext("service return Array<GenotypeType>")
+      .toBe(result);
+  });    
+
+  it("should return Array<Individual> (/individuals)", () => {
+    const result: Array<Individual> = individualOutputMockup.individuals;
+
+    expect(service.getIndividuals())
+      .withContext("service return Array<Individual>")
+      .toBe(result);
+  });   
+
+  it("should return Array<Effect> (/effects)", () => {
+    const result: Array<Effect> = effectOutputMockup.effects;
+
+    expect(service.getEffects())
+      .withContext("service return Array<Individual>")
+      .toBe(result);
+  }); 
+  
+  it("should return Array<Population> (/populations)", () => {
+    const result: Array<Population> = populationOutputMockup.populations;
+
+    expect(service.getPopulations())
+      .withContext("service return Array<Population>")
+      .toBe(result);
+    expect(result[0].id)
+      .withContext("first Population must have id 1")
       .toBe(1);
-    expect(loginServiceSpy.login.calls.mostRecent().returnValue)
-      .toBe(status);
-  });*/
+    expect(result[result.length-1].id)
+      .withContext("last Population must have id 12")
+      .toBe(12);
+  });  
+  
+  it("should return Page<Gene> (/genes)", () => {
+    const resultContent: Array<Gene> = geneOutputMockup.genes;
+
+    const result = service.getGenes();
+
+    expect(result.content)
+      .withContext("result.content return Array<Gene>")
+      .toBe(resultContent);
+    expect(result.totalElements)
+      .withContext("result.content return Array<Gene>")
+      .toBe(21085);  
+    expect(result.totalPages)
+      .withContext("result.content return Array<Gene>")
+      .toBe(1055);              
+  });  
+
+  it("should return Page<Gene> filtered by query (/genes)", () => {
+    const geneModel: GeneParams = geneSingleParameterInputMockup;
+    const resultContent: Array<Gene> = geneFilteredOutputMockup.genes;
+
+    const result = service.getGenes(geneModel);
+
+    expect(result.content)
+      .withContext("result.content return Array<Gene>")
+      .toBe(resultContent);
+    expect(result.totalElements)
+      .withContext("result.content return Array<Gene>")
+      .toBe(1);  
+    expect(result.totalPages)
+      .withContext("result.content return Array<Gene>")
+      .toBe(1);              
+  });  
+
+  it("should return Page<Gene> filtered by query (/genes)", () => {
+    const geneModel: GeneParams = geneMultipleParametersInputMockup;
+    const resultContent: Array<Gene> = geneFilteredOutputMockup.genes;
+
+    const result = service.getGenes(geneModel);
+
+    expect(result.content)
+      .withContext("result.content return Array<Gene>")
+      .toBe(resultContent);
+    expect(result.totalElements)
+      .withContext("result.content return Array<Gene>")
+      .toBe(1);  
+    expect(result.totalPages)
+      .withContext("result.content return Array<Gene>")
+      .toBe(1);              
+  });   
 });
---
+++ src/fixtures/index.js
@@ -0,0 +1,1 @@
+export * from './varcanService/login/input';
