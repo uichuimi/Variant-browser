0ccf34b268d65cf9b6212b557432825a6b7d4230
Jaime <jaime99dr@gmail.com>
2022-04-24 20:39:34 +0100

    VB-101 routing to login, register and variants done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,31 @@
+// MODULES
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
-import { TableModule } from 'primeng/table';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputTextModule } from 'primeng/inputtext';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { ListboxModule } from 'primeng/listbox';
-import { ChipsModule } from 'primeng/chips';
-import { InputNumberModule } from 'primeng/inputnumber';
-
-import { AppRoutingModule } from './app-routing.module';
+// COMPONENTS
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
 import { TableComponent } from './components/table/table.component';
 import { TableContentComponent } from './components/table/table-content/table-content.component';
 import { TableFooterComponent } from './components/table/table-footer/table-footer.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     TableComponent,
     TableContentComponent,
     TableFooterComponent,
+    RegisterComponent,
   ],
   imports: [
-    FormsModule,
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
-    TableModule,
-    ButtonModule,
-    CardModule,
-    DropdownModule,
-    InputTextModule,
-    MultiSelectModule,
-    ListboxModule,
-    ChipsModule,
-    InputNumberModule,
-    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
