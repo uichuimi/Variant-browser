de46d76fdf5e0259fd097aaa56153483ac03e543
Jaime <jaime99dr@gmail.com>
2022-04-28 22:58:29 +0100

    VB-110 errors animations

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 // SERVICES
 import { VarCanService } from 'src/app/services/varcan-service/var-can.service';
@@ -8,7 +15,36 @@ import { VarCanService } from 'src/app/services/varcan-service/var-can.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('passwordError', [
+      state('closed', style({
+        opacity: 0,
+        transform: "translateX(-10px)"
+      })),
+      state('open', style({
+        opacity: 1,
+        transform: "translateX(0)"
+      })),
+      transition('* => *', animate(300))
+    ]),
+    trigger('inputError', [
+      transition(
+        ':enter', 
+        [
+          style({ height: 0, opacity: 0 }),
+          animate('1s ease-out', style({ transform: "translateY(0)", opacity: 1 }))
+        ]
+      ),
+      transition(
+        ':leave', 
+        [
+          style({ height: 300, opacity: 1 }),
+          animate('1s ease-in', style({ transform: "translateY(-10px)", opacity: 0 }))
+        ]
+      )      
+    ])
+  ]  
 })
 export class LoginComponent implements OnInit {
   private service: VarCanService;
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -5,8 +5,7 @@ import {
   state,
   style,
   animate,
-  transition,
-  // ...
+  transition
 } from '@angular/animations';
 
 @Component({
@@ -24,7 +23,23 @@ import {
         transform: "translateX(0)"
       })),
       transition('* => *', animate(300))
-    ])
+    ]),
+    trigger('inputError', [
+      transition(
+        ':enter', 
+        [
+          style({ height: 0, opacity: 0 }),
+          animate('1s ease-out', style({ transform: "translateY(0)", opacity: 1 }))
+        ]
+      ),
+      transition(
+        ':leave', 
+        [
+          style({ height: 300, opacity: 1 }),
+          animate('1s ease-in', style({ transform: "translateY(-10px)", opacity: 0 }))
+        ]
+      )      
+    ])    
   ]
 })
 export class RegisterComponent {
