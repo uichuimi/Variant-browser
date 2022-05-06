037c746cae2c07205c993289c7526f11e30024b6
Jaime <jaime99dr@gmail.com>
2022-05-06 11:20:06 +0100

    VB-110 properties filter

--- src/app/components/properties-filter/properties-filter.component.ts
+++ src/app/components/properties-filter/properties-filter.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./properties-filter.component.css']
 })
 export class PropertiesFilterComponent implements OnInit {
-
-  constructor() { }
+  fieldOptions: Array<Object> = [
+    { type: 'number', name: 'Start' },
+    { type: 'number', name: 'End' },
+    { type: 'number', name: 'GMAF' },
+    { type: 'number', name: 'Polyphen' },
+    { type: 'number', name: 'Sift' },
+    { type: 'list', name: 'Chromosomes' },
+    { type: 'list', name: 'Genes' },
+    { type: 'list', name: 'Effects' },
+    { type: 'list', name: 'Impacts' },
+    { type: 'list', name: 'Biotypes' },
+    { type: 'list', name: 'Identifiers' }
+  ];
 
   ngOnInit(): void {
   }
 
+  addFilter(propertiesFilter) {
+    console.log("propertiesFilter: ", propertiesFilter.value.field.type);
+  }
 }
