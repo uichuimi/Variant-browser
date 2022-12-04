93bf220e56f6e694da0f8eecc28a0b5936d8c423
David Medina <david00medina@gmail.com>
2022-12-04 11:46:57 +0000

    dev Project update & Table Component modifications: [-] Eliminated unnecessary methods in Table Component [-] Cleaned HTML in Table Component [~] Version change in angular.json [~] Packages version change in package.json

---
+++ apps/variant-browser/src/app/models/data-structure/VariantLine.ts
@@ -0,0 +1,12 @@
+export interface VariantLine {
+  id?: number;
+  sndId?: string;
+  region?: string;
+  variant?: string;
+  gene?: string;
+  consequence?: string;
+  impact?: string;
+  dp?: string;
+  frequency?: string;
+  gmaf?: string
+}
